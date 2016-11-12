7f25a09bd2ae54048480a34532576e62d3b20f13
John Anderson <jander43@vols.utk.edu>
2016-11-12 09:12:01 -0500

    Added problem 3.5 sort stack

---
+++ python/ch3/sort_stack.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Write a program to sort a stack suck that the smalles items are on the top.
+You can use an additional temporary stack, but you may not copy the elements
+into any other data structure (such as an array). The stack supports the
+following operations: push, pop, peek, and isEmpty.
+Complexity: TODO
+Usage: TODO
+'''
