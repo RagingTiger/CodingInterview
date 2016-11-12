f6e01638413e711ae6fc0219b751f4701ec5ebf7
John Anderson <jander43@vols.utk.edu>
2016-11-12 09:10:42 -0500

    Added problem 4.2 minimal tree

---
+++ python/ch4/minimal_tree.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Given a sorted (increasing order) array with unique integer elements, write
+an algorithm to create a binary search tree with minimal height.
+Complexity: TODO
+Usage: TODO
+'''
