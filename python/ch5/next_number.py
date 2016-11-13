dc0757268bddfe9fe42c8917b463d2e9b7f761b4
John Anderson <jander43@vols.utk.edu>
2016-11-13 07:49:48 -0500

    Added problem 5.4 next number

---
+++ python/ch5/next_number.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Given a positive integer, print the next smallest and the next largest
+number that have the same number of 1 bits in their binary representation.
+Complexity: TODO
+Usage: TODO
+'''
