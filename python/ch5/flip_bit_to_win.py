e2ea45f78b6740574d0000be10784774295a6db5
John Anderson <jander43@vols.utk.edu>
2016-11-13 07:49:24 -0500

    Added problem 5.3 flip bit to win

---
+++ python/ch5/flip_bit_to_win.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    You have an integer you can flip exactly one bit from a 0 to a 1. Write
+code to find the length of the longest sequence of 1s you could create.
+EXAMPLE
+Input:    1775   (or: 11011101111)
+Output:   8
+Complexity: TODO
+Usage: TODO
+'''
