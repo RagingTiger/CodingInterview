3b23bd83a5810bd7f6ee9ab5b135d1aaa14c0c4e
John Anderson <jander43@vols.utk.edu>
2016-11-13 15:12:15 -0500

    Added time complexity

--- python/ch3/sort_stack.py
+++ python/ch3/sort_stack.py
@@ -8,7 +8,7 @@ Problem Statement:
 top. You can use an additional temporary stack, but you may not copy the
 elements into any other data structure (such as an array). The stack supports
 the following operations: push, pop, peek, and isEmpty.
-Complexity: TODO
+Complexity: O(Nlog(N))
 Usage:
     sort_stack case1
     sort_stack case2
