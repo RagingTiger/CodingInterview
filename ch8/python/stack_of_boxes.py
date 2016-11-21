b11241880c45a213fd79d2843562bf672531a4d5
John Anderson <jander43@vols.utk.edu>
2016-11-20 22:17:23 -0500

    Initial problem statement

---
+++ ch8/python/stack_of_boxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    You have a stack of n boxes, with widths W, heights H, and depths D. The
+boxes cannot be rotated and can only be stacked on top one another if each box
+in the stack is strictly larger than the box above it in width, height, and
+depth. Implement a method to compute the height of the tallest possible stack.
+The height of a stack is the sum of the heights of each box.
+Complexity: TODO
+Usage: TODO
+'''
