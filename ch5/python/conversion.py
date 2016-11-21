03a11db1f23e8a6fa6c020361cce75560babe228
John Anderson <jander43@vols.utk.edu>
2016-11-20 22:16:58 -0500

    Initial problem statement

---
+++ ch5/python/conversion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Write a function to determine the number of bits you would need to flip to
+convert integer A to integer B.
+EXAMPLE
+Input:      29 (or: 11101), 15 (or: 01111)
+Output:     2
+Complexity: TODO
+Usage: TODO
+'''
