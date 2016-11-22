96572cc82c7d98a233decfccefe70eb4e0131840
John Anderson <jander43@vols.utk.edu>
2016-11-21 20:08:07 -0500

    Initial template

---
+++ ch4/python/paths_with_sum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    You are given a binary tree in which each node contains an integer value
+(which might be positive or negative). Design an algorithm to count the number
+of paths that sum to a given value. The path does not need to start on an end
+at the root or a leaf, but it must go downwards (traveling only from parent
+nodes to child nodes).
+Complexity: TODO
+Usage: TODO
+'''
