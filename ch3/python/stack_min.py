887d82af5eb3c30275c243bbb8776638eb47a887
John Anderson <jander43@vols.utk.edu>
2016-11-24 09:10:44 -0500

    Initial problem statement

---
+++ ch3/python/stack_min.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    How would you design a stack which, in addition to push() and pop(), has a
+function min() which returns the minimum element? push(), pip(), and min() all
+operate in O(1) time.
+Complexity: O(1)
+Usage: TODO
+'''
