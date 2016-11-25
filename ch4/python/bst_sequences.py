c040e9c1751e5aaadfab581d166b07cbc8bff458
John Anderson <jander43@vols.utk.edu>
2016-11-24 20:46:52 -0500

    Initial problem statement

---
+++ ch4/python/bst_sequences.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    A binary search tree was created by traversing through an array from left
+to right and inserting each element. Given a binary search tree with distinct
+elements, print all possible arrays that could have led to this tree.
+EXAMPLE
+Input:
+                        ( 2 )
+                        /   \
+                    ( 1 )   ( 3 )
+
+
+Output: (2, 1, 3), (2, 1, 3)
+Complexity: TODO
+Usage: TODO
+'''
