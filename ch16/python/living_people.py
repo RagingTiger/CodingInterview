428e9ff464ffa7c066183f38f5aeae2fe3bb0cfe
John Anderson <jander43@vols.utk.edu>
2016-11-25 22:08:25 -0500

    Initial problem statement

---
+++ ch16/python/living_people.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Given a list of people with their birth and death years, implement a method
+to compute the year with the most number of people alive. You may assume that
+all people were born between 1900 and 2000 (inclusive). If a person was alive
+during any portion of that year, they should be included in that year's count.
+For example, person(birth=1908, death=1909) is included in the counts for both
+1908 and 1909.
+Complexity: TODO
+Usage: TODO
+'''
