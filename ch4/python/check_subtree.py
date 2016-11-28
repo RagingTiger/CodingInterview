904a51356a50839a7285b031fd15ed79e69f2db4
John Anderson <jander43@vols.utk.edu>
2016-11-27 22:14:40 -0500

    Initial Problem Statement

---
+++ ch4/python/check_subtree.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    T1 and T2 are two very large binary trees, with T1 much bigger than T2.
+Create an algorithm to determine if T2 is a subtree of T1. A tree T2 is a
+subtree of T1 if there exists a node N in T1 such that the subtree of N is
+identical to T2. That is, if you cut off the tree at node N, the two trees
+would be identical.
+Complexity: TODO
+Usage: TODO
+'''
