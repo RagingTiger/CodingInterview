7c3b4975a4456621f4cab19378d1b699c8be22ff
John Anderson <jander43@vols.utk.edu>
2016-11-27 22:13:56 -0500

    Initial Problem Statement

---
+++ ch10/python/search_in_rotated_array.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Given a sorted array of N integers that has been rotated an unknown number
+of times, write code to find an element in the array. You may assume that the
+array was originally sorted in increasing order.
+Complexity: TODO
+Usage: TODO
+'''
