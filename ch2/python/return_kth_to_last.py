26058ee0e8dd7e83c0f15219f3d94af1917fee3f
John Anderson <jander43@vols.utk.edu>
2016-11-28 20:49:06 -0500

    Initial problem statement

---
+++ ch2/python/return_kth_to_last.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Implement an algorithm to find the kth to last element of a singly linked
+list.
+Complexity: TODO
+Usage: TODO
+'''
