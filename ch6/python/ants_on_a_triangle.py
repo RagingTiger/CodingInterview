a9665a1cb624d2f57830e89bae576eeda04906ce
John Anderson <jander43@vols.utk.edu>
2016-11-28 20:49:25 -0500

    Initial problem statement

---
+++ ch6/python/ants_on_a_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    There are three ants on different vertices of a triangle. What is the
+probability of collision (between any two or all of them) if they start walking
+on the sides of the triangle? Assume that each ant randomly picks a direction,
+with either direction being equally likely to be chosen, and that they walk at
+the same speed. Similarly, find the probability of collision with N ants on an
+N-vertex polygon.
+Complexity: TODO
+Usage: TODO
+'''
