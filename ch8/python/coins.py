66c3398435d39cf7aba05bc83e60974c8320ae7d
John Anderson <jander43@vols.utk.edu>
2016-12-01 22:35:34 -0500

    Initial Problem Statement

---
+++ ch8/python/coins.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Given an infinite number of quarters (25 cents), dimes (10 cents), nickels
+(5 cents), and pennies (1 cent), write code to calculate the number of ways of
+representing N cents.
+Complexity: TODO
+Usage: TODO
+'''
