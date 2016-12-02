adf3fceb461c18a485ab73b74ed835b536578a4d
John Anderson <jander43@vols.utk.edu>
2016-12-01 22:35:09 -0500

    Initial Problem Statement

---
+++ ch10/python/sorted_matrix_search.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Given MxN matrix in which each row and each column is sorted in ascending
+order write a method to find an element
+Complexity: TODO
+Usage: TODO
+'''
