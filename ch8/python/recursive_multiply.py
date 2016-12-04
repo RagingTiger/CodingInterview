9d7809c5ef1fe23e8f2a8507a49c3c0f0bcdf449
John Anderson <jander43@vols.utk.edu>
2016-12-03 20:33:47 -0500

    Initial problem statement

---
+++ ch8/python/recursive_multiply.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Andersonn
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Write a recursive function to multiply two positive integers without using
+the ‘*’ operator. You can use addition, subtraction, and bit shifting, but you
+should minimize the number of those operations.
+Complexity: TODO
+Usage: TODO
+'''
