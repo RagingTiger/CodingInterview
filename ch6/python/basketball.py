2458fd3e377a8a4e98672bfd9d9dc5f9dcf8d30d
John Anderson <jander43@vols.utk.edu>
2016-12-04 09:32:26 -0500

    Initial problem statement

---
+++ ch6/python/basketball.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    You have a basketball hoop and someone says that you can play one of two
+games:
+        Game1: You get one shot to make the hoop
+        Game2: You get three shots and you have to make two of three shots
+If p is the probability of making a particular shot, for which values of p
+should you pick one game or the other?
+Complexity: TODO
+Usage: TODO
+'''
