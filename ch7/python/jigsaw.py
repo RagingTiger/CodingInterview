b82997a4a587840d299ce096f1ad06f3291d1503
John Anderson <jander43@vols.utk.edu>
2016-12-04 20:00:02 -0500

    Initial problem statement

---
+++ ch7/python/jigsaw.py
@@ -0,0 +1,12 @@
+
+'''
+Author: John D. Anderson
+Email: jander43@vols.utk.edu
+Problem Statement:
+    Implement an NxN jigsaw puzzle. Design the data structures and explain an
+algorithm to solve the puzzle. You can assume that you have a fitsWith method
+which, when passed two puzzle edges, returns true if the two edges belong
+together.
+Complexity: TODO
+Usage: TODO
+'''
