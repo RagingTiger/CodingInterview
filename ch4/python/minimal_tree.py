a2d2557ce3fb0f494b0c2b527be168755cec4687
John Anderson <jander43@vols.utk.edu>
2016-12-13 22:42:13 -0500

    Adding initial python solution

--- ch4/python/minimal_tree.py
+++ ch4/python/minimal_tree.py
@@ -6,6 +6,69 @@ Email: jander43@vols.utk.edu
 Problem Statement:
     Given a sorted (increasing order) array with unique integer elements, write
 an algorithm to create a binary search tree with minimal height.
-Complexity: TODO
-Usage: TODO
+Complexity: O(N)
+Usage: minimal_tree
 '''
+
+
+# functions
+def answer():
+    pass
+
+
+# classes
+class Tree(object):
+    '''
+    Class to implement a binary tree
+    '''
+    # constructor
+    def __init__(self, array):
+
+        self.root_node = recursive_tree_build(array)
+
+    # internal method
+    def _recursive_build_tree(self, array):
+
+        # get length
+        length = len(array)
+
+        # check
+        if not length:
+            return None
+
+        # calculate root
+        root = length / 2
+
+        # create Node
+        node = Node(array[root])
+
+        # get left and right node
+        node.left = self._recursive_build_tree(array[0:root])
+        node.right = self._recursive_build_tree(array[root:length-1])
+
+        # return
+        return node
+
+
+class Node(object):
+    '''
+    Class to implement node for binary tree
+    '''
+    # constructor
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+# executable
+if __name__ == '__main__':
+
+    # executable import only
+    from docopt import docopt
+
+    # get args
+    args = docopt(__doc__)
+
+    # run
+    answer()
